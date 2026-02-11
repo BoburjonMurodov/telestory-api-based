3afb9e225ba2a935c031cbcbc60c245548c0f099
Boboor <boburjonmurodov06@gmail.com>
2026-02-11 13:47:25 +0500

    fix: Correct format specifier order in UZ/RU story count messages

--- internal/models/user.go
+++ internal/models/user.go
@@ -10,7 +10,7 @@ type User struct {
 	FirstName         string       `json:"first_name"`
 	LastName          string       `json:"last_name"`
 	Username          string       `json:"username"`
-	PhoneNumber       string       `json:"phone_number"`  // New field
+	PhoneNumber       string       `json:"phone_number"`
 	LanguageCode      string       `json:"language_code"` // Treated as empty string if NULL
 	IsTelegramPremium bool         `json:"is_telegram_premium"`
 	PremiumExpiresAt  sql.NullTime `json:"premium_expires_at"`
